b10c646387f1b7e8b7287f205d5dca91c6780d35
No767 <73260931+No767@users.noreply.github.com>
2021-08-21 13:57:26 -0700

    Revert back changes

--- Django/Deltaline/settings.py
+++ Django/Deltaline/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = 'django-insecure-3+8#4ekdhgdv)sg4%x1bomz_zcbei0=+ktwzn#yi00ony=i9ab'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
