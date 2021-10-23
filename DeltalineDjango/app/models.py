9f98f519db72351c5b0d76e0ca6a0d98636adb7d
No767 <winterpiece104@gmail.com>
2021-10-23 13:50:31 -0700

    Add a working Django App

--- DeltalineDjango/app/models.py
+++ DeltalineDjango/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class Comments(models.Model):
+	username = models.CharField(max_length=15)
+	time = models.DateTimeField()
+	description = models.TextField()
