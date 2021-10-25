eaceec75a5ecd8f0441e78771d42b9a0c146553d
No767 <winterpiece104@gmail.com>
2021-10-24 23:15:24 -0700

    Fix Issue with Pipfile

--- DeltalineDjango/app/urls.py
+++ DeltalineDjango/app/urls.py
@@ -3,10 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    
-]
-=======
     path("", views.index, name="index"),
 ]
->>>>>>> 1ddb8e6213f41cb1f69a6c50691889cff5e7eda1
--- DeltalineDjango/app/views.py
+++ DeltalineDjango/app/views.py
@@ -1,12 +1,2 @@
 from django.http import HttpResponse
-<<<<<<< HEAD
-=======
 from django.shortcuts import render
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
-# Create your views here.
->>>>>>> 1ddb8e6213f41cb1f69a6c50691889cff5e7eda1
