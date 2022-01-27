f2c72d9a447bc1d46f4eb381ba3e1dc93e98f150
No767 <73260931+No767@users.noreply.github.com>
2022-01-26 23:33:25 -0800

    Remove Flask + Django and Replace with Sanic

--- DeltalineDjango/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Comments(models.Model):
-    username = models.CharField(max_length=15)
-    time = models.DateTimeField()
-    description = models.TextField()
--- DeltalineDjango/app/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    text = "This is only for testing"
-    return HttpResponse(text)
