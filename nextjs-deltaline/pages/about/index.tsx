0688d772a22ce2f664350e3cbc55fd900380ef2f
No767 <73260931+No767@users.noreply.github.com>
2022-04-14 23:14:30 -0700

    Optimize Load Times by Globallly Loading All Fonts + MantineProviders

--- nextjs-deltaline/pages/about/index.tsx
+++ nextjs-deltaline/pages/about/index.tsx
@@ -1,35 +1,18 @@
 import { Body } from "../../lib/about-components/body";
 import { Profile } from "../../lib/about-components/profile";
-import type { NextPage } from "next";
-import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
 import { Navbar } from "../../lib/index-components/navbar";
 import { Footer } from "../../lib/index-components/footer";
+import type { NextPage } from "next";
 
 const About: NextPage = () => {
   return (
-    <MantineProvider
-      theme={{
-        fontFamily: "Quicksand",
-        colorScheme: "dark",
-        headings: { fontFamily: "Quicksand" },
-      }}
-      withGlobalStyles
-    >
-      <div>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <Navbar />
-        <Profile />
+    <div>
+      <Navbar />
+      <Profile />
 
-        <Body />
-        <Footer />
-      </div>
-    </MantineProvider>
+      <Body />
+      <Footer />
+    </div>
   );
 };
 
--- nextjs-deltaline/pages/index.tsx
+++ nextjs-deltaline/pages/index.tsx
@@ -3,32 +3,15 @@ import { Footer } from "./../lib/index-components/footer";
 import { HeroHeader } from "./../lib/index-components/hero-header";
 import { Navbar } from "./../lib/index-components/navbar";
 import type { NextPage } from "next";
-import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
 
 const Home: NextPage = () => {
   return (
-    <MantineProvider
-      theme={{
-        fontFamily: "Quicksand",
-        colorScheme: "dark",
-        headings: { fontFamily: "Quicksand" },
-      }}
-      withGlobalStyles
-    >
-      <div>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <Navbar />
-        <HeroHeader />
-        <Body />
-        <Footer />
-      </div>
-    </MantineProvider>
+    <div>
+      <Navbar />
+      <HeroHeader />
+      <Body />
+      <Footer />
+    </div>
   );
 };
 
