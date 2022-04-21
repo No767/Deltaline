304677b080ca3e6e22d149e222f05aeeb643e780
No767 <73260931+No767@users.noreply.github.com>
2022-04-21 00:16:10 -0700

    Add more pics

--- nextjs-deltaline/pages/japanese-2/index.tsx
+++ nextjs-deltaline/pages/japanese-2/index.tsx
@@ -1,30 +1,36 @@
-import type { NextPage } from "next";
+import { HeroHeader } from "../../lib/japanese-2-components/hero-header";
 import { NavbarMain } from "../../lib/index-components/navbar-main";
+import type { NextPage } from "next";
 
-import { Group, Title, BackgroundImage } from "@mantine/core";
+import {
+  Group,
+  Title,
+  Text,
+  Stack,
+  Image,
+  Divider,
+  Anchor,
+} from "@mantine/core";
 
 const japanese2: NextPage = () => {
   return (
     <div>
       <NavbarMain />
-      <BackgroundImage
-        src="https://raw.githubusercontent.com/No767/Deltaline/dev/site/css/assets/background.webp"
-        sx={{
-          height: 350,
-        }}
-      >
-        <Group align="center" direction="column" spacing="xs" mx={50}>
-          <Title
-            align="center"
-            sx={{
-              color: "#F8F9FA",
-            }}
-            pt={150}
-          >
-            Japanese 2
-          </Title>
-        </Group>
-      </BackgroundImage>
+      <HeroHeader />
+      <Group align="center" direction="column" spacing="xs" mx={50} pt={25}>
+        <Text size="xl" color="white">
+          こんにちは！
+        </Text>
+        <Text size="xl" color="white">
+          初めまして！
+        </Text>
+        <Text size="xl" color="white">
+          私の名前はノエルです！
+        </Text>
+        <Text size="xl" color="white">
+          どうぞよろしくお願いします。
+        </Text>
+      </Group>
     </div>
   );
 };
